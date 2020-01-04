e0b6f0d998c9c4d6fad7c50247b7d9805140d709
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-01-04 15:12:24 -0500

    updating JS file and some CSS

--- script.js
+++ script.js
@@ -8,54 +8,60 @@ var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
 var counter = document.getElementById("counter");
 var userScore = document.getElementById("score");
-var resultContainer = document.getElementById("result");
+var resultContainer = document.getElementById("resultBox");
+var result = document.getElementById("result");
+var resultIncorect = document.getElementById("resultIncorect");
+var resultAnswer = document.getElementById("corectAnswer");
+var userName = document.querySelector("#userName");
+var submit = document.getElementById("#submit");
+
 // create questions inside the array
 var questions = [
 { question:"Which is the correct syntax to use javascript file ?",
-    choiceA:"Wrong",
-    choiceB:"Correct",
-    choiceC:"Worng",
-    choiceD:"Worng",
+    choiceA:"<script src=xxx.js>",
+    choiceB:"<script href=xxx.js>",
+    choiceC:"<script path=xxx.js>",
+    choiceD:"<script link=xxx.js>",
     correct:"B"
 },
 {
     question:"How we can declare a (function) in Javascript ?",
-    choiceA:"Correct",
-    choiceB:"Wrong",
-    choiceC:"Wrong",
-    choiceD:"Wrong",
+    choiceA:"function myFunction(...)",
+    choiceB:"myFunction function(...)",
+    choiceC:"declare function myFunction(...)",
+    choiceD:"var function myFunction(...)",
     correct:"A"
 },
 {
     question:"Which is correct way to use (If) in javascript ?",
-    choiceA:"Wrong",
-    choiceB:"Correct",
-    choiceC:"Wrong",
-    choiceD:"Wrong",
+    choiceA:"(if ...){...}",
+    choiceB:"if(...){...}",
+    choiceC:"call if (...) {...}",
+    choiceD:"Wif{...}(...)",
     correct:"B"
 },
 {
     question:"How to find out the max of x and y ?",
-    choiceA:"Wrong",
-    choiceB:"Wrong",
-    choiceC:"Correct",
-    choiceD:"Wrong",
+    choiceA:"ceil(x, y)",
+    choiceB:"max(x, y)",
+    choiceC:"Math.max(x, y)",
+    choiceD:"top(x, y)",
     correct:"C"
 },
 {
     question:"How can we declare an object with Javascript ?",
-    choiceA:"Worng",
-    choiceB:"Worng",
-    choiceC:"Correct",
-    choiceD:"Worng",
+    choiceA:"var variable = new Object()",
+    choiceB:"var variable = Object()",
+    choiceC:"var variable = {}",
+    choiceD:"var variable = new myFunction()",
     correct:"C"
 },
 {
     question:"How can we open new window with Javascript ?",
-    choiceA:"Correct",
-    choiceB:"Worng",
-    choiceC:"Worng",
-    choiceD:"Worng",
+    choiceA:"window.open(...)",
+    choiceB:"window.new(...)",
+    choiceC:"open(new window())",
+    choiceD:"window.open_new(...)",
     correct:"A"
 },
    
@@ -76,11 +82,11 @@ var incorrect = 0;
 // create a function to render a question
 function renderQuestion(){
     var q = questions[runningQuestion];
-    question.innerHTML ="<P>"+q.question+"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+    question.textContent ="<P>"+q.question+"</p>";
+    choiceA.textContent = q.choiceA;
+    choiceB.textContent = q.choiceB;
+    choiceC.textContent = q.choiceC;
+    choiceD.textContent = q.choiceD;
 
 };
 
@@ -105,16 +111,30 @@ TIMER = setInterval(renderCounter,1000); //100 ms == 1sec
 
 function renderCounter(){
     if(count <= questionTime){
-        counter.innerHTML = "Timer: "+count;
+        counter.textContent = "Timer: "+count;
         count++;
         localStorage.setItem("counter",count);
+
+        userScore.textContent ="Your Score: "+score;
+        localStorage.setItem("score",score);
 
     }
     else{
         count = 0;
         clearInterval(TIMER);
         scoreRender();
+        
     }
+    // if we want to have time for each question
+
+    // if(runningQuestion<lastQuestion){
+    //     runningQuestion++;
+    //     renderQuestion();
+    // }
+    // else{
+    //     clearInterval(TIMER);
+    //     scoreRender();
+    // }
 
 };
 
@@ -126,13 +146,15 @@ function checkAnswer(answer){
         localStorage.setItem("score",score);
 
     }
-    else{ incorrect++ }
+    else{ incorrect++ };
+    // if we want to reset the time after each question we can add here count = 0 ; 
 
     if(runningQuestion<lastQuestion){
         runningQuestion++;
         renderQuestion();
     }
     else{
+        // end the quiz
         clearInterval(TIMER);
         scoreRender();
     }
@@ -147,16 +169,33 @@ function answerIsCorrect(){
 // answer is Wrong
 function answerIsWrong(){
     document.querySelectorAll(runningQuestion).style.backgroundColor = "#f00";
-}
+};
 
 //create a function to show the score at the end
 function scoreRender(){
+    quiz.style.display ="none";
+
     resultContainer.style.display="block";
+    // calculate the user answer percent
+    var scorePercent = Math.round(100 * score/questions.length);
+    result.innerHTML ="<P>"+"Your score percent : "+ scorePercent +"% " +"</p>";
+    resultIncorect.innerHTML ="<P>"+"Your Incorect Answer : "+ incorrect +"</p>";
+    resultAnswer.innerHTML ="<P>"+"Your Corect Answer : "+ score +"</p>";
+
+   
+
+};
+  
+// submit.addEventListener("click", function(event) {
+//     event.preventDefault();
     
+// var user = userName.value.trim();
+// localStorage.setItem("name", user);
+// //  user = localStorage.getItem("name");
 
-}
+// localStorage.setItem("name", JSON.stringify(user));
+// new String(user);
 
-
-
-
+// });
+// console.log(localStorage("name"));
 
